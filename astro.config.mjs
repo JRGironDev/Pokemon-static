fb8a08c0d285005a37a4c3d7a097635296b5fe76
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-05 16:05:49 -0600

    Agregar soporte para favoritos de Pokémon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import starlight from '@astrojs/starlight';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [starlight({
@@ -24,5 +28,5 @@ export default defineConfig({
                 autogenerate: { directory: 'reference' },
             },
         ],
-		}), tailwind()],
+        }), tailwind(), icon(), solidJs()],
 });
