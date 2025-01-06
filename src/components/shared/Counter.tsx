f0623221c7e02625a8b9ce249b9a84e108c24e94
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-05 19:57:21 -0600

    Agregar componente de contador a la página de favoritos

---
+++ src/components/shared/Counter.tsx
@@ -0,0 +1,20 @@
+import { createSignal } from "solid-js"
+
+export const Counter = () => {
+
+    const [counter, setCounter] = createSignal(10)
+
+    return (
+        <>
+        <h1>Counter</h1>
+        <h3 class="mb-3">Value: {counter()}</h3>
+
+        <button
+            onClick={() => setCounter(prev => ++prev)}
+            class="bg-blue-500 p-2 mr-2 rounded-full">+1</button>
+        <button
+            onClick={() => setCounter(prev => --prev)}
+            class="bg-blue-500 p-2 mr-2 rounded-full">-1</button>
+        </>
+    )
+}
